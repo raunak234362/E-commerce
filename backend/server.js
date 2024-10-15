4dfab7780f90e70a59681a49f480c6da7998a437
raunak234362 <raunakdob@gmail.com>
2024-10-15 14:25:49 +0530

    Add product route and controller
    - Added a new route and controller for handling product-related requests.
    - Created a new file `productRoute.js` in the `routes` directory.
    - Implemented the `getAllProducts` function in the `productController.js` file.
    - The route is accessible at `/api/v1/products` and returns a JSON response with a success message.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const app = require("./app")
 const dotenv =require("dotenv");
+const connectDatabse= require("./config/database")
+
 
 //config
 dotenv.config({path:"backend/config/config.env"})
 
-
+////Connect Database
+connectDatabse();
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port http://localhost:${process.env.PORT}`)
