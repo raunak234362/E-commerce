ae958afab412d29fb67fcd43e81c9534a89a4f1c
raunak234362 <raunakdob@gmail.com>
2024-10-15 16:47:58 +0530

    Refactor database connection and add product CRUD operations

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
+const dotenv = require("dotenv")
+
 const connectDatabase = () => {
-  mongoose
-    .connect("process.env.DB_URI", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      userCreateIndex: true,
-    })
+  mongoose.connect("mongodb+srv://raunak:qwerty!23456@cluster0.vwjkk.mongodb.net/")
+  // mongoose.connect("mongodb://localhost:27017/Ecommerce")
     .then((data) => {
       console.log(`Mongodb connected with server: ${data.connection.host}`);
     })
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,58 @@
-exports.getAllProducts= (req,res)=>{
-    res.status(200).json({message:"Route us working fine"})
+const Product = require("../models/productModel");
+
+//Create Product -- Admin
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+// Get all product
+exports.getAllProducts = async (req, res) => {
+  const product = await Product.find();
+  res.status(200).json({ success: true, product });
+};
+
+
+//Update Product-- Admin
+exports.updateProduct = async(req,res)=>{
+    const product = Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+      new:true,
+      runValidators:true,
+      useFindAndModify:false
+    })
+
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
+
+
+//Delete Product-- Admin
+exports.deleteProduct = async(req,res)=>{
+    const product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+
+    await product.remove();
+
+    res.status(200).json({
+        success:true,
+        message:"Product is deleted"
+    })
 }
