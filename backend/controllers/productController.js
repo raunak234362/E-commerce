0d86d73fab38f4e11e29916c8ef31f6052131a2d
Raunak <raunakdob@gmail.com>
2024-11-08 18:42:37 +0530

    Add error handling middleware and refactor product update logic

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -17,13 +17,10 @@ exports.getAllProducts = async (req, res) => {
 
 
 //Update Product-- Admin
-exports.updateProduct = async(req,res)=>{
+exports.updateProduct = async(err,req,res,next)=>{
     const product = Product.findById(req.params.id);
     if(!product){
-        return res.status(500).json({
-            success:false,
-            message:"Product not found"
-        })
+        return next(new ErrorHandler("Product not found",404));
     }
 
     product = await Product.findByIdAndUpdate(req.params.id,req.body,{
